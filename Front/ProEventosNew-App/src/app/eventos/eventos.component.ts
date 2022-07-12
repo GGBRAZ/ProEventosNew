36f3ee4fbb2f67b65aab9fba1d88a657860e6ed1
Guilherme Gomes do Braz <ggbraz@gmail.com>
2022-07-12 11:27:45 -0300

    Desenvolvendo mecanismo de busca

--- Front/ProEventosNew-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventosNew-App/src/app/eventos/eventos.component.ts
@@ -1,4 +1,5 @@
 import { HttpClient } from '@angular/common/http';
+import { ThisReceiver } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./eventos.component.scss'],
 })
 export class EventosComponent implements OnInit {
-  public eventos: any;
+  public eventos: any = [];
+  public eventosFiltrados: any = [];
+  widthImg: number = 170;
+  marginImg: number = 2;
+  showImage: boolean = true;
+  private _filterList: string = '';
+
+  public get filtroLista(): string {
+    return this._filterList;
+  }
+
+  public set filtroLista(value: string) {
+    this._filterList = value;
+    this.eventosFiltrados = this.filtroLista
+      ? this.filtrarEventos(this.filtroLista)
+      : this.eventos;
+  }
+
+  filtrarEventos(filtrarPor: string): any {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: any) =>
+        evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+        evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    );
+  }
 
   constructor(private http: HttpClient) {}
 
@@ -15,10 +41,18 @@ export class EventosComponent implements OnInit {
     this.getEventos();
   }
 
+  toggleImage() {
+    this.showImage = !this.showImage;
+  }
+
   public getEventos(): void {
     this.http.get('https://localhost:5001/api/eventos').subscribe(
-      response => (this.eventos = response),
-      error => console.log(error)
+      (response) => {
+        (this.eventos = response);
+        this.eventosFiltrados = this.eventos;
+
+      },
+      (error) => console.log(error)
     );
   }
 }
